ae567a476259c0af4e175469831cd64621811c82
zmmyslony <zmmyslony@gmail.com>
2024-10-04 12:01:14 +0100

    Added more commands to the BasePrinter

--- lib/gcode/base_printer.py
+++ lib/gcode/base_printer.py
@@ -5,6 +5,10 @@ from lib.pattern_reading.print_path import PrintPath
 from lib.pattern_reading.pattern import Pattern
 import time
 
+VER_MAJOR = 0
+VER_MINOR = 1
+VER_PATCH = 0
+
 
 class BasePrinter:
     current_position = np.array([0, 0, 0], dtype=np.float32)
@@ -21,7 +25,7 @@ class BasePrinter:
                  non_print_speed,
                  print_width,
                  layer_thickness,
-                 first_layer_height,
+                 first_layer_height=None,
                  physical_pixel_size=None,
                  lift_off_distance=10,
                  lift_off_height=2):
@@ -29,17 +33,37 @@ class BasePrinter:
         self.non_print_speed = non_print_speed
         self.print_width = print_width
         self.layer_thickness = layer_thickness
-        self.first_layer_thickness = first_layer_height
+        self.first_layer_thickness = layer_thickness if first_layer_height is None else first_layer_height
 
         self.physical_pixel_size = physical_pixel_size
         self.lift_off_distance = lift_off_distance
         self.lift_off_height = lift_off_height
-        self.__generate_header()
+        self._generate_header()
         self.start_time = time.time()
+
+    def export(self, filename, header_supplement=None, body_supplement=None, footer_supplement=None):
+        seconds = int(self.print_time * 60) % 60
+        minutes = int(self.print_time) % 60
+        hours = int(self.print_time / 60) % 60
+
+        self._comment_header(f"Total printing time: {hours:d}:{minutes:d}:{seconds:d} (excluding heating).")
+        self._comment_header(f"Total printing distance: {self.print_distance:.1f} mm at {self.print_speed} mm/min.")
+        self._comment_header(f"Total non-printing distance: {self.non_print_distance:.1f} mm at {self.non_print_speed} mm/min.")
+
+        f = open(filename, 'w')
+        f.write(self.header)
+        if header_supplement is not None: f.write(header_supplement)
+        f.write("\n")
+        f.write(self.body)
+        if body_supplement is not None: f.write(body_supplement)
+        f.write("\n")
+        f.write(self.footer)
+        if footer_supplement is not None: f.write(footer_supplement)
+        f.close()
 
     def slice_pattern(self, pattern: Pattern, layers, **kwargs):
         self.physical_pixel_size = self.print_width / pattern.pixel_path_width
-        self.comment(f"Slicing pattern \"{pattern.pattern_name}\"")
+        self._comment_body(f"Slicing pattern \"{pattern.pattern_name}\"")
         pattern_copy = deepcopy(pattern)
         pattern_copy.scale(self.physical_pixel_size)
 
@@ -48,90 +72,159 @@ class BasePrinter:
         else:
             i_layers = [i % pattern_copy.layer_count for i in range(layers)]
 
-        self.__z_move(kwargs.get('first_layer_thickness', self.first_layer_thickness))
+        self._z_move(kwargs.get('first_layer_thickness', self.first_layer_thickness))
 
-        for i in i_layers:
-            self.slice_layer(pattern_copy.layers[i])
-            self.__z_move_incremental(self.layer_thickness)
+        for i, i_layer in enumerate(i_layers):
+            self.slice_layer(pattern_copy.layers[i_layer])
+            self.interlayer_function(i, **kwargs)
+            self._z_move_incremental(self.layer_thickness)
 
         print(
             f"Generation of gcode for {len(i_layers)}-layered \"{pattern_copy.pattern_name}\" took "
             f"{(time.time() - self.start_time) * 1e3:.2f} ms.")
 
-    def __generate_header(self):
-        time_string = time.strftime("%a, %d %b %Y %H:%M:%S")
-        self.header = "; Generated on: " + time_string + "\n"
-        self.header += "; Using code version: \n"
+    def interlayer_function(self, layer_index, **kwargs):
+        """
+        Placeholder function for child classes to introduce specific interlayer behaviour.
+        :return:
+        """
+        return
 
-    def comment(self, content):
+    def _generate_header(self):
+        time_string = time.strftime("%a, %d %b %Y %H:%M:%S")
+        self._comment_header(f"File generated using VectorSlicerGCode version: {VER_MAJOR}.{VER_MINOR}.{VER_PATCH}.")
+        self._comment_header(f"Generated on: {time_string}.")
+
+    def _break_header(self):
+        self.header += "\n"
+
+    def _break_body(self):
+        self.body += "\n"
+
+    def _comment_body(self, content):
         self.body += "; " + content + "\n"
 
-    def slice_layer(self, layer: Layer):
+    def _comment_header(self, content):
+        self.header += "; " + content + "\n"
+
+    def _comment_footer(self, content):
+        self.footer += "; " + content + "\n"
+
+    def _command_body(self, content):
+        self.body += content + "\n"
+
+    def _command_header(self, content):
+        self.header += content + "\n"
+
+    def _command_footer(self, content):
+        self.footer += content + "\n"
+
+    def slice_layer(self, layer: Layer, speed=None):
         if self.physical_pixel_size is None:
             raise ValueError("Physical pixel size is undefined. Initialise it either by providing it during "
                              "initialisation or by slicing using a Pattern class object.")
 
-        self.comment("Beginning a new layer.")
+        self._comment_body("Beginning a new layer.")
         for path in layer.print_paths:
-            self.comment("Moving to the next path.")
-            self.body += self.__non_printing_move(path.start())
-            self.body += self.__slice_path(path)
+            self._comment_body("Moving to the next path.")
+            self._non_printing_move(path.start())
+            self._slice_path(path, speed=speed)
 
-    def __z_move(self, height):
+    def _z_move(self, height, speed=None):
         new_position = copy(self.current_position)
         new_position[2] = height
-        self.__printing_move(new_position)
+        self._printing_move(new_position, speed=speed)
 
-    def __z_move_incremental(self, displacement):
-        self.__z_move(self.current_position[2] + displacement)
+    def _z_move_incremental(self, displacement, speed=None):
+        self._z_move(self.current_position[2] + displacement, speed)
 
-    def __printing_move_2d(self, position):
+    def _printing_move_2d(self, position, speed=None):
         self.current_position[:2] = position
-        return f"G1 X{position[0]:.3f} Y{position[1]:.3f} F{self.print_speed:d}\n"
+        self._command_body(
+            f"G1 X{position[0]:.3f} Y{position[1]:.3f} F{self.print_speed if speed is None else speed:d}")
 
-    def __printing_move_3d(self, position):
+    def _printing_move_2d_relative(self, displacement, speed=None):
+        self._printing_move_2d(self.current_position[:2] + np.array(displacement), speed=speed)
+
+    def _printing_move_3d(self, position, speed=None):
         self.current_position = position
-        return f"G1 X{position[0]:.3f} Y{position[1]:.3f} Z{position[2]:.3f} F{self.print_speed:d}\n"
+        self._command_body(
+            f"G1 X{position[0]:.3f} Y{position[1]:.3f} Z{position[2]:.3f} F{self.print_speed if speed is None else speed:.0f}")
 
-    def __printing_move(self, position):
+    def _printing_move(self, position, speed=None):
         if len(position) == 2:
-            return self.__printing_move_2d(position)
+            return self._printing_move_2d(position, speed=speed)
         elif len(position) == 3:
-            return self.__printing_move_3d(position)
+            return self._printing_move_3d(position, speed=speed)
         else:
             raise RuntimeError(f"Invalid position given: {position} should have either 2 or 3 components.")
 
-    def __non_printing_move(self, position):
+    def _non_printing_move(self, position, speed=None):
+        if speed is None: speed = self.non_print_speed
+        position = np.array(position)
         if len(position) == 2:
             position = np.array([position[0], position[1], self.current_position[2]])
         elif len(position) != 3:
             raise RuntimeError(f"Invalid position given: {position} should have either 2 or 3 components.")
 
-        if self.lift_off_distance is not None and np.linalg.norm(
-                position - self.current_position) >= self.lift_off_distance:
-            command = f"G1 Z{self.current_position[2] + self.lift_off_height:.3f} F{self.non_print_speed:d}\n"
+        if (
+                self.lift_off_distance is not None and
+                np.linalg.norm(position - self.current_position) >= self.lift_off_distance and
+                position[2] == self.current_position[2]
+        ):
+            self._command_body(f"G0 Z{self.current_position[2] + self.lift_off_height:.3f} F{speed:d}")
             move_length = self.lift_off_height
 
-            command += f"G1 X{position[0]:.3f} Y{position[1]:.3f} F{self.non_print_speed:d}\n"
+            self._command_body(f"G0 X{position[0]:.3f} Y{position[1]:.3f} F{speed:d}")
             move_length += np.linalg.norm(self.current_position[:2] - position[:2])
 
-            command += f"G1 Z{position[2]:.3f} F{self.non_print_speed:d}\n"
+            self._command_body(f"G0 Z{position[2]:.3f} F{speed:d}")
             move_length += np.abs(self.current_position[2] + self.lift_off_height - position[2])
         else:
-            command = f"G1 X{position[0]:.3f} Y{position[1]:.3f}  Z{position[2]:.3f}F{self.non_print_speed:d}\n"
+            self._command_body(f"G0 X{position[0]:.3f} Y{position[1]:.3f} Z{position[2]:.3f} F{speed:d}")
             move_length = np.linalg.norm(self.current_position - position)
         self.current_position[:len(position)] = position
         self.non_print_distance += move_length
-        self.print_time += move_length / self.non_print_speed
+        self.print_time += move_length / speed
 
-        return command
+    def _non_printing_move_relative(self, displacement, speed=None):
+        displacement = np.array(displacement)
+        if len(displacement) == 2:
+            displacement = np.concatenate([displacement, [0]])
+        self._non_printing_move(self.current_position + displacement, speed=speed)
 
-    def __slice_path(self, path: PrintPath):
-        path_string = ''
+    def _set_relative_positioning(self):
+        self._comment_body("Switching to relative positioning.")
+        self._command_body("G91")
+
+    def _set_absolute_positioning(self):
+        self._comment_body("Switching to absolute positioning.")
+        self._command_body("G90")
+
+    def _home_2d(self):
+        self._break_body()
+        self._comment_body("Homing.")
+        self._set_relative_positioning()
+        self._non_printing_move([0, 0, 10])
+        self._set_absolute_positioning()
+        self._command_body("G28 X0 Y0")
+        self.current_position[0] = 0
+        self.current_position[1] = 0
+
+    def _home_3d(self):
+        self._break_body()
+        self._comment_body("Homing.")
+        self._set_relative_positioning()
+        self._non_printing_move([0, 0, 10])
+        self._set_absolute_positioning()
+        self._command_body("G28 X0 Y0 Z0")
+        self.current_position = np.array([0, 0, 0])
+
+    def _slice_path(self, path: PrintPath, speed=None):
+        if speed is None: speed = self.print_speed
 
         for position in path.path_coordinates:
-            path_string += self.__printing_move(position)
+            self._printing_move(position, speed=speed)
 
-        self.print_time += path.length / self.print_speed
+        self.print_time += path.length / speed
         self.print_distance += path.length
-        return path_string
