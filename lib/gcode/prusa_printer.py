fef176395ff1cd1aefa7a6a2381cae489f65f3ed
zmmyslony <zmmyslony@gmail.com>
2024-10-07 12:12:04 +0100

    New class which will use PrusaSlicer generated headers and footers and generate the movement commands by itself, allowing for variable width printing using thermoplastics.

---
+++ lib/gcode/prusa_printer.py
@@ -0,0 +1,10 @@
+from lib.gcode.base_printer import BasePrinter, ExtrusionTypes
+import numpy as np
+
+
+class PrusaPrinter(BasePrinter):
+    def __init__(self, print_speed=2400, non_print_speed=18000, print_width=0.8, layer_thickness=0.2,
+                 lift_off_distance=5, lift_off_height=0.8, filament_diameter=1.75):
+        BasePrinter.__init__(self, print_speed, non_print_speed, print_width, layer_thickness,
+                             lift_off_distance=lift_off_distance, lift_off_height=lift_off_height,
+                             filament_diameter=filament_diameter)
